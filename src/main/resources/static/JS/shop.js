1b15590799ccd15304125443a52a7b1c699970b5
jaimachu <jaimeruedacarpintero@gmail.com>
2023-03-25 23:22:06 +0100

    Actualización shop HTML y CSS y creación de add_new_product HTML y CSS

---
+++ src/main/resources/static/JS/shop.js
@@ -0,0 +1,19 @@
+function filtrar(){
+    let tags = document.getElementsByClassName('tag');
+    let products = document.getElementsByClassName("product");
+    let select = document.getElementById('filter_type_product');
+    let opciones = select.querySelectorAll('option');
+    let textSelected = opciones[select.selectedIndex].textContent;
+    
+    for(let i=0; i<tags.length; i++){
+        let textTag = tags[i].textContent;
+        if (textTag == textSelected){
+            products[i].style.display = 'none';
+        }
+        console.log(textTag);
+    }
+}
+let button = document.getElementById("support_button");
+button.addEventListener("click", function(){
+    window.location.replace("../HTML/support.html");
+});
