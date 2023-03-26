561529c437089a710a799270f05a6f0a68790877
jaimachu <116104294+jaimachu@users.noreply.github.com>
2023-03-26 18:57:31 +0200

    Creación JS para navegación por páginas y actualizaciones pequeñas en add_new_product y shop

--- src/main/resources/static/JS/shop.js
+++ src/main/resources/static/JS/shop.js
@@ -7,13 +7,9 @@ function filtrar(){
     
     for(let i=0; i<tags.length; i++){
         let textTag = tags[i].textContent;
-        if (textTag == textSelected){
+        if (textTag != textSelected){
             products[i].style.display = 'none';
         }
         console.log(textTag);
     }
 }
-let button = document.getElementById("support_button");
-button.addEventListener("click", function(){
-    window.location.replace("../HTML/support.html");
-});
