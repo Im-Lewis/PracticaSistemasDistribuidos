e89f7b9ae3b971a663634b0b1aca3180a0496c8e
Im-Lewis <luisovejeromrtn@gmail.com>
2023-04-11 21:19:27 +0200

    Various Updates

--- src/main/resources/static/JS/change_page.js
+++ src/main/resources/static/JS/change_page.js
@@ -1,85 +1,62 @@
 /*-------------------------------------------------------------------------------------------------------------------*/
+let buttonSup = document.getElementById('support_button');
+let buttonShop = document.getElementById('shop_button');
 let buttonLogin = document.getElementById("log_in_button");
+let buttonSignin = document.getElementById("sign_in_button");
+let buttonTournaments = document.getElementById("tournaments_button");
+let buttonMultimedia = document.getElementById("multimedia_button");
+let buttonHome = document.getElementById("home_button");
+let buttonGoBack = document.getElementById("go_back_btn");
+let buttonAddProduct = document.getElementById("button_add_new_product");
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonShop.addEventListener("click", function(){
+    window.location.replace('../shop');
+});
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonSup.addEventListener("click", function(){
+    window.location.replace("../HTML/support.html");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
 buttonLogin.addEventListener("click", function(){
     window.location.replace("../HTML/login.html");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonSignin = document.getElementById("sign_in_button");
+
 buttonSignin.addEventListener("click", function(){
     window.location.replace("../HTML/signin.html");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonTournaments = document.getElementById("tournaments_button");
 buttonTournaments.addEventListener("click", function(){
     window.location.replace("../view/tournaments");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonMultimedia = document.getElementById("multimedia_button");
 buttonMultimedia.addEventListener("click", function(){
     window.location.replace("../HTML/multimedia.html");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonHome = document.getElementById("home_button");
 buttonHome.addEventListener("click", function(){
     window.location.replace("../index.html");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonGoBack = document.getElementById("go_back_btn");
 buttonGoBack.addEventListener("click", function(){
     window.location.replace("../view/tournaments");
 });
 
 /*-------------------------------------------------------------------------------------------------------------------*/
-
-
-
-let buttonSup = document.getElementById("support_button");
-buttonSup.addEventListener("click", function(){
-    window.location.replace("../HTML/support.html");
-});
-
 buttonSup.addEventListener("click", function() {
-    setTimeout(function() {
     window.location.replace("../HTML/support.html");
-    }, 3000); // El número 3000 representa el tiempo en milisegundos (3 segundos)
 });
 
-let buttonShop = document.getElementById("shop_button");
-buttonShop.addEventListener("click", function(){
-    window.location.replace("../shop");
-});
-
-/*buttonSup.addEventListener("click", function(){
-    setTimeout(function() {
-        window.location.replace("../HTML/support.html");
-    }, 3000);
-});*/
-
-/*
-let buttonShop = document.getElementById("shop_button");
-buttonShop.addEventListener("click", function(){
-    window.location.replace("../HTML/shop.html");
-});
-*/
-
-let buttonAddProduct = document.getElementById("button_add_new_product");
+/*-------------------------------------------------------------------------------------------------------------------*/
 buttonAddProduct.addEventListener("click", function(){
     window.location.replace("../HTML/add_new_product.html");
 });
 
-
-/*let buttonRegister = document.getElementById("signin_button");
-buttonRegister.addEventListener("click", function(){
-    window.location.replace("../HTML/signin.html");
-});
-
-let buttonLogIn = document.getElementById("login_button");
-buttonLogIn,addEventListener("click", function(){
-    this.window.location.replace("../HTML/login.html");
-})*/
 
--- src/main/resources/static/JS/tournaments.js
+++ src/main/resources/static/JS/tournaments.js
@@ -3,19 +3,3 @@ let buttonCreateTournament = document.getElementById("create_tournament_btn");
 buttonCreateTournament.addEventListener("click", function(){
     window.location.replace("../HTML/add_new_tournaments.html");
 });
-
-/*-------------------------------------------------------------------------------------------------------------------*/
-/*
-let buttonEditTournament = document.getElementById("create_tournament_btn");
-buttonEditTournament.addEventListener("click", function(){
-    window.location.replace("");
-});
-*/
-/*-------------------------------------------------------------------------------------------------------------------*/
-/*
-let buttonDeleteTournament = document.getElementById("create_tournament_btn");
-buttonDeleteTournament.addEventListener("click", function(){
-    window.location.replace("");
-});
-*/
-/*-------------------------------------------------------------------------------------------------------------------*/
