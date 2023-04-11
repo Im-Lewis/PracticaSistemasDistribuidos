e30fec76e12521839d7d3b8887099ccce8e0f8c9
jaimachu <jaimeruedacarpintero@gmail.com>
2023-04-12 00:25:30 +0200

    Eliminado basic_styles2.css

--- src/main/java/es/ssdd/practica/User/UserRestController.java
+++ src/main/java/es/ssdd/practica/User/UserRestController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/rest/users")
+@RequestMapping("/api/users")
 @RestController
 public class UserRestController {
     @Autowired
