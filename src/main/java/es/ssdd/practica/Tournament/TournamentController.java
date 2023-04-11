9b1ed859a7325774bdbc107a32219e3a840d14ce
Im-Lewis <luisovejeromrtn@gmail.com>
2023-04-12 00:33:39 +0200

    Various changes

--- src/main/java/es/ssdd/practica/Tournament/TournamentController.java
+++ src/main/java/es/ssdd/practica/Tournament/TournamentController.java
@@ -4,6 +4,7 @@ import es.ssdd.practica.EntitiesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,5 +33,11 @@ public class TournamentController {
         Tournament tournament = entitiesService.getTournamentById(num);
         model.addAttribute("tournament", tournament);
         return "view_tournament";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteTournament(Model model, @PathVariable Long id){
+        Tournament tournament = entitiesService.deleteTournament(id);
+        return "tournament_deleted_succesfully";
     }
 }
--- src/main/resources/static/JS/tournaments.js
+++ src/main/resources/static/JS/tournaments.js
@@ -1,5 +1,43 @@
 /*-------------------------------------------------------------------------------------------------------------------*/
-let buttonCreateTournament = document.getElementById("create_tournament_btn");
+let buttonCreateTournament = document.getElementById('create_tournament_btn');
+let buttonDeleteTournament = document.getElementById('delete_tournament_button');
+let buttonGoBackOrganizer = document.getElementById('go_back_btn_organizer');
+let buttonGoBackUser = document.getElementById('go_back_btn_user');
+let buttonCreateOrganizer = document.getElementById('create_organizer_btn');
+let buttonCreateUser = document.getElementById('create_user_btn');
+
+/*-------------------------------------------------------------------------------------------------------------------*/
 buttonCreateTournament.addEventListener("click", function(){
     window.location.replace("../HTML/add_new_tournaments.html");
 });
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonDeleteTournament.addEventListener("click", function(){
+    window.location.replace("../delete/{id}");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonGoBackOrganizer.addEventListener("click", function(){
+    window.location.replace("../HTML/add_new_organizer.html");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonGoBackUser.addEventListener("click", function(){
+    window.location.replace("../HTML/add_new_user.html");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonCreateOrganizer.addEventListener("click", function(){
+    window.location.replace("../HTML/add_new_organizer.html");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+buttonCreateUser.addEventListener("click", function(){
+    window.location.replace("../HTML/add_new_user.html");
+});
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+
+
+
